72b97db5e8d6e07452beb5adb792335cfba67bb3
MonsterNone <alwaysalittlemonster@gmail.com>
2019-05-01 18:39:47 +0800

    修改判断值，解决了入点出点判断不准的问题
    将颜色出现的次数由1改为200（150？忘记了）

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-# 测试
+﻿# -*- coding: UTF-8 -*-
 # 通过最小和最大的r/g/b分量计算y=kx+b公式然后替换
 
 from PIL import Image, ImageCms
@@ -17,7 +17,6 @@ def convert_to_srgb(icc_profile, img):
     return img
     
 
-
 def single_np(arr, target):
     arr = np.array(arr)
     mask = (arr == target)
@@ -31,18 +30,18 @@ def reverse(p):
     x1 = np.min(ap)
     x2 = np.max(ap)
 
-    '''
     while True:
-        if single_np(ap, x1) <= 1:
-            x1 = x1 + 1
+        if single_np(ap, x1) <= 100 :
+            x1 = x1 + 2
         else:
+            print('x1 =', x1)
             break
     while True:
-        if single_np(ap, x2) <= 1:
+        if single_np(ap, x2) <= 100:
             x2 = x2 - 1
         else:
+            print('x2 =', x2)
             break
-    '''
     
     y1 = 255
     y2 = 0
@@ -51,12 +50,14 @@ def reverse(p):
     f_b = y1 - f_k * x1
 
     tmp = ap * f_k + f_b
+
     return tmp
 
 
 def process(file_name):
     src = Image.open('in/' + file_name)
     icc_profile = src.info.get('icc_profile', '')
+    
     r, g, b = src.split()
     tmp_r = reverse(r)
     tmp_g = reverse(g)
@@ -67,10 +68,10 @@ def process(file_name):
     b = Image.fromarray(tmp_b).convert('L')
 
     img = Image.merge('RGB', (r, g, b))
-    
+
     if icc_profile:
+        print('色彩空间转换...')
         img = convert_to_srgb(icc_profile, img)
-        
     img.save('out/' + file_name[:-4] + '.jpg',
              format = "JPEG",
              quality = 100,
@@ -78,7 +79,7 @@ def process(file_name):
              dpi=(300,300))
 
 
-print('本程序会读取in文件夹中的所有.tif文件并输出到out文件夹中。按回车继续')
+print('本程序会读取in文件夹中的所有.tif文件并输出到out文件夹中。按任意键继续')
 input()
 
 for i in os.listdir('in/'):
@@ -86,6 +87,7 @@ for i in os.listdir('in/'):
         continue
     print('处理', i)
     process(i)
-
-print('\n全部完成！按回车关闭程序。')
+    print()
+    
+print('全部完成！按任意键关闭程序。')
 input()
